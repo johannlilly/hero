debb1b025768f46457bd06d8cc8b77997ab6b672
jlilly <>
2017-08-18 17:49:43 -0400

    create class account

---
+++ hero-app/hero-app/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User
+{
+    public abstract class Account
+    {
+        #region Username
+        private string _Username;
+        public string Username
+        {
+            get { return _Username; }
+            set { _Username = value; }
+        }
+        #endregion Username
+        #region Password
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; }
+        }
+        #endregion Password
+        #region StartDate
+        private DateTime _StartDate;
+        public DateTime StartDate
+        {
+            get { return _StartDate; }
+            set { _StartDate = value; }
+        }
+        #endregion
+
+    }
+    public class Teacher : Account
+    {
+        public byte Rating()
+        {
+            byte rating = new Byte();
+            return rating;
+        }
+    }
+    public class Student : Account
+    {
+
+    }
+}
